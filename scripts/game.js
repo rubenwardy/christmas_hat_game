089c1d6b6c1f3d45221b41b6dc2a06cb2c021fb9
rubenwardy <rubenwardy@gmail.com>
2014-12-19 18:12:25 +0000

    Update

--- scripts/game.js
+++ scripts/game.js
@@ -11,7 +11,7 @@ var top_score = null;
 // Initialise Game
 function init()
 {
-	for (var y = 0; y < engine.ce.height - 100; y += 5) {
+	for (var y = 0; y < engine.ce.height - 100; y += 20) {
 		snow.push({x: Math.random() * engine.ce.width, y: y + (Math.random() - 0.5) * 10, vx: (Math.random() - 0.5) * 50 });
 		snow.push({x: Math.random() * engine.ce.width, y: y + (Math.random() - 0.5) * 10, vx: (Math.random() - 0.5) * 50 });
 		snow.push({x: Math.random() * engine.ce.width, y: y + (Math.random() - 0.5) * 10, vx: (Math.random() - 0.5) * 50 });
@@ -27,37 +27,39 @@ function levelUp() { level++; }
 function tick(ce, c, dtime)
 {
 	var now = new Date().getTime();
-	if (now > last_snow + 10) {
+	if (now > last_snow + 120) {
 		last_snow = now;
-		snow.push({x: Math.random() * ce.width, y: 0, vx: (Math.random() - 0.5) * 50 });
-		snow.push({x: Math.random() * ce.width, y: 0, vx: (Math.random() - 0.5) * 50 });
-		snow.push({x: Math.random() * ce.width, y: 0, vx: (Math.random() - 0.5) * 50 });
-		snow.push({x: Math.random() * ce.width, y: 0, vx: (Math.random() - 0.5) * 50 });
-		snow.push({x: Math.random() * ce.width, y: 0, vx: (Math.random() - 0.5) * 50 });
+		snow.push({x: Math.random() * ce.width, y: 0, vx: (Math.random() - 0.5) * 30 });
+		snow.push({x: Math.random() * ce.width, y: 0, vx: (Math.random() - 0.5) * 30 });
+		snow.push({x: Math.random() * ce.width, y: 0, vx: (Math.random() - 0.5) * 30 });
+		snow.push({x: Math.random() * ce.width, y: 0, vx: (Math.random() - 0.5) * 30 });
+		snow.push({x: Math.random() * ce.width, y: 0, vx: (Math.random() - 0.5) * 30 });
 	}
 
 	if (lifes > 0) {
 		if (level == 1) {
 			if (now > since_last + 1000) {
+				var spawn_width = 400;
 				since_last = now;
-				balls.push({x: Math.random() * 400 + ce.width/2 - 200, y: 0, v: {x: 0, y: 0}, id: "blue", score: 1});
+				balls.push({x: Math.random() * spawn_width + ce.width/2 - spawn_width/2, y: 0, v: {x: 0, y: 0}, id: "red", score: 1});
 			}
 			if (score > 20)
 				levelUp();
 		} else if (level == 2) {
 			if (now > since_last + 1000) {
+				var spawn_width = 600;
 				since_last = now;
 				if (Math.random() > 0.6)
-					balls.push({x: Math.random() * 400 + ce.width/2 - 200, y: 0, v: {x: 0, y: 10}, id: "brussel", score: 0, no_drop_penalty: true, hp: -1});
+					balls.push({x: Math.random() * spawn_width + ce.width/2 - spawn_width/2, y: 0, v: {x: 0, y: 10}, id: "brussel", score: 0, no_drop_penalty: true, hp: -1});
 				else
-					balls.push({x: Math.random() * 400 + ce.width/2 - 200, y: 0, v: {x: 0, y: 10}, id: "blue", score: 1});
+					balls.push({x: Math.random() * spawn_width + ce.width/2 - spawn_width/2, y: 0, v: {x: 0, y: 10}, id: "red", score: 1});
 			}
 		}
 	}
 
 	var g = c.createLinearGradient(0, 2 * ce.height / 3, 0, ce.height);
-	g.addColorStop(0, "#636eca");
-	g.addColorStop(1, "#9375b1");
+	g.addColorStop(0, "#26478b");
+	g.addColorStop(1, "#4163A4");
 	c.fillStyle = g;
 	c.fillRect(0, 0, ce.width, ce.height);
 
@@ -66,19 +68,19 @@ function tick(ce, c, dtime)
 		c.fillStyle = "white";
 		c.fillRect(one.x, one.y, 3, 3);
 		one.x += one.vx * dtime;
-		one.y += 200 * dtime;
+		one.y += 100 * dtime;
 		if (one.y > ce.height - 100) {
 			snow.splice(i, 1);
 			i--;
 		}
 	}
 
-	var g = c.createLinearGradient(0, ce.height - 100, 0, ce.height);
+	var g = c.createLinearGradient(0, ce.height - 50, 0, ce.height);
 	g.addColorStop(0, "rgba(255, 255, 255, 0)");
-	g.addColorStop(0.07, "white");
+	g.addColorStop(0.1, "white");
 	g.addColorStop(1, "#ccc");
 	c.fillStyle = g;
-	c.fillRect(0, ce.height - 100, ce.width, ce.height);
+	c.fillRect(0, ce.height - 50, ce.width, ce.height);
 
 	for (var i = 0; i < balls.length; i++) {
 		var ball = balls[i];
@@ -87,8 +89,8 @@ function tick(ce, c, dtime)
 		ball.y   += ball.v.y * dtime;
 		ball.v.y += 9.8 * 50 * dtime;
 
-		if (lifes > 0 && ball.y > ce.height - 260 && !ball.failed) {
-			if (Math.abs(ball.x - paddle) < 132/2) {
+		if (lifes > 0 && ball.y > ce.height - 210 && !ball.failed) {
+			if (Math.abs(ball.x - paddle) < 56) {
 				score += ball.score;
 				if (ball.hp)
 					lifes += ball.hp;
@@ -101,7 +103,7 @@ function tick(ce, c, dtime)
 			}
 		}
 
-		if (ball.y > ce.height - 90) {
+		if (ball.y > ce.height - 70) {
 			if (ball.x > ce.width/2)
 				ball.v.x += 5;
 			else
@@ -122,7 +124,7 @@ function tick(ce, c, dtime)
 			paddle = 101;
 		if (paddle > ce.width - 100)
 			paddle = ce.width - 101;
-		c.drawImage($("#hat")[0], paddle - 132/2, ce.height - 260);
+		c.drawImage($("#hat")[0], paddle - 132/2, ce.height - 210);
 	}
 
 	c.fillStyle = "white";
@@ -131,7 +133,6 @@ function tick(ce, c, dtime)
 	c.font = "16px Arial";
 	c.fillText("Level " + level, 10, 60);
 	c.fillText(lifes + " lifes remaining", 10, 80);
-
 	if (lifes <= 0)
 		c.fillText("You died, reload the page to restart.", 20, ce.height / 2 - 5);
 	else if (score == 0)
